61baff30e8c6583f678401e4664756ba7c761299
OssamaTammam <ossamatammam@gmail.com>
2023-07-04 01:16:09 +0300

    ignores

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://snfygjrohlkwjzgkoefg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuZnlnanJvaGxrd2p6Z2tvZWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc1NDY4NzMsImV4cCI6MjAwMzEyMjg3M30.c_EfUpDD4Sjt5iHXTwfN2wafWNA8MOLvOhh2i280PRs";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
